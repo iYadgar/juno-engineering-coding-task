01e5890a384fa387eae8ae44554a3c6bf7ce6d8c
idanyadgar <yadgaridan@gmail.com>
2022-05-05 19:57:28 +0300

    - finished ecommerce

--- src/task2/ecommerce.js
+++ src/task2/ecommerce.js
@@ -1,42 +1,71 @@
 ////////////////////////////////////////////// Helper code, do not edit /////////////////////////////////////////
-import { allIds, fetchOrderById } from "../api";
+import {allIds, fetchOrderById} from "../api/index.js";
+import {isBefore}               from "date-fns";
 
 ////////////////////////////////// Your code tasks is below //////////////////////////////////////////////////////
 
-const fetchAllOrders = () => {
-    const ids = allIds;
-    // .....
-    //   1. TODO: fetch all ids using the "fetchOrderById" and the given ids, make it work as efficient and clean as possible.
+const getUniqueArrayFromObjectsArray = (array, key) => Array.from(new Set(array.map((value) => value[key])))
+
+export const fetchAllOrders = async () => {
+	const ids = allIds;
+	// .....
+	//   1. TODO: fetch all ids using the "fetchOrderById" and the given ids, make it work as efficient and clean as possible.
+	const ordersPromises = ids.map((id) => fetchOrderById(id))
+	return Promise.all(ordersPromises)
 };
 
-const bucketOrdersByUsers = () => {
-    let ordersByUsers = {};
-    //   2. TODO: using the function from section 1 you should now bucket the orders by user.
-    // each key in the object (ordersByUsers) represents a userId and each value is an array of the orders of that user.
-    return ordersByUsers;
+export const bucketOrdersByUsers = async (orders = []) => {
+	//   2. TODO: using the function from section 1 you should now bucket the orders by user.
+	// each key in the object (ordersByUsers) represents a userId and each value is an array of the orders of that user.
+	const allOrders = orders.length ? orders : await fetchAllOrders()
+	const uniqueUserIds = getUniqueArrayFromObjectsArray(allOrders, 'userId')
+	return uniqueUserIds.reduce((acc, userId) => {
+		acc[userId] = allOrders.filter((order) => order.userId === userId)
+		return acc
+	}, {});
 };
 
-const getLast2WeeksOrders = () => {
-    //   3. TODO: fetch all Ids and return array with only the last 2 weeks orders. make it work as efficient and clean as possible.
+export const getLast2WeeksOrders = async (orders = []) => {
+	//   3. TODO: fetch all Ids and return array with only the last 2 weeks orders. make it work as efficient and clean as possible.c
+	const allOrders = orders.length ? orders : await fetchAllOrders()
+	const twoWeeksAgo = new Date()
+	twoWeeksAgo.setDate(twoWeeksAgo.getDate() - 14)
+	return allOrders.filter((order) => !isBefore(order.timestamp, twoWeeksAgo))
 };
 
-const bucketOrdersByDate = () => {
-    let ordersByDate = {};
-    //   4. TODO: using the function from section 3 bucket the orders by date.
-    // each key in the object (ordersByDate) represents a day and each value is an array of the orders in that date.
-    return ordersByDate;
+export const bucketOrdersByDate = async () => {
+	//   4. TODO: using the function from section 3 bucket the orders by date.
+	// each key in the object (ordersByDate) represents a day and each value is an array of the orders in that date.
+	const lastTwoWeeksOrders = await getLast2WeeksOrders()
+	const uniqueTimestamps = getUniqueArrayFromObjectsArray(lastTwoWeeksOrders, 'timestamp')
+	return uniqueTimestamps
+		.reduce((acc, timestamp) => {
+					acc[new Date(timestamp).toDateString()] = lastTwoWeeksOrders.filter((order) => order.timestamp === timestamp)
+					return acc
+				}
+			, {});
 };
 
-fetchAllOrders();
-// .then(console.log);
 
-bucketOrdersByUsers();
-// .then(console.log);
+// fetchAllOrders()
+// 	.then((data) => {
+// 		console.log(data);
+// 		console.log(`****** fetchAllOrders  ******`);
+// 	});
+// bucketOrdersByUsers()
+// 	.then((data) => {
+// 		console.log(data);
+// 		console.log(`****** bucketOrdersByUsers  ******`);
+// 	});
+// getLast2WeeksOrders()
+// 	.then((data) => {
+// 		console.log(data);
+// 		console.log(`****** getLast2WeeksOrders  ******`);
+// 	});
+bucketOrdersByDate()
+	.then((data) => {
+		console.log(data);
+		console.log(`****** bucketOrdersByDate  ******`);
+	});
 
-getLast2WeeksOrders();
-// .then(console.log);
-
-bucketOrdersByDate();
-// .then(console.log);
-
-////////////////////////////////////////
+//////////////////////////////////////
--- src/task2/ecommerce.test.js
+++ src/task2/ecommerce.test.js
@@ -1,7 +1,37 @@
-import { fetchOrderById } from "../api";
+import {allIds, fetchOrderById}                                                       from "../api";
+import {fetchAllOrders, bucketOrdersByUsers, getLast2WeeksOrders, bucketOrdersByDate} from "./ecommerce.js";
+import {isBefore}                                                                     from "date-fns";
 
 const ORDER_ID = "70ef599e5eca171b2bce84d1"
+let MOCK_ORDERS = []
+beforeAll(async () => MOCK_ORDERS = await fetchAllOrders())
 test("Ecommerce - fetchOrderById", async () => {
-    let orders = await fetchOrderById(ORDER_ID);
-    expect(orders).toBeTruthy();
+	let orders = await fetchOrderById(ORDER_ID);
+	expect(orders).toBeTruthy();
 });
+test("Ecommerce - fetchAllOrders", async () => {
+	const allOrders = await fetchAllOrders()
+	expect(allIds.length === allOrders.length).toBeTruthy()
+})
+test("Ecommerce - bucketOrdersByUsers", async () => {
+	const orderByUser = await bucketOrdersByUsers()
+	const singleOrder = await fetchOrderById(ORDER_ID)
+	expect(orderByUser[singleOrder.userId].length).toBeTruthy()
+})
+test("Ecommerce - getLast2WeeksOrders", async () => {
+	const lastToWeeksOrder = await getLast2WeeksOrders()
+	const twoWeeksAgo = new Date()
+	twoWeeksAgo.setDate(twoWeeksAgo.getDate() - 14)
+	expect(lastToWeeksOrder.filter((order) => isBefore(order.timestamp, twoWeeksAgo)).length).toBeFalsy()
+})
+test("Ecommerce - bucketOrdersByDate", async () => {
+	const ordersByDate = await bucketOrdersByDate(MOCK_ORDERS)
+	const twoWeeksAgo = new Date()
+	twoWeeksAgo.setDate(twoWeeksAgo.getDate() - 14)
+	const newOrder = MOCK_ORDERS.find((order) => !isBefore(order.timestamp, twoWeeksAgo))
+	const oldOrder = MOCK_ORDERS.find((order) => isBefore(order.timestamp, twoWeeksAgo))
+	if (newOrder){
+	expect(ordersByDate[new Date(newOrder.timestamp).toDateString()]?.length).toBeTruthy()
+	}
+	expect(ordersByDate[new Date(oldOrder.timestamp).toDateString()]?.length).toBeFalsy()
+})
